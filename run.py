5920741b96fe42667359693be5506cf164e64181
akimitsukimiya <akimitsukimiya@gmail.com>
2020-06-24 18:00:16 +0800

    delete unwanted

--- run.py
+++ run.py
@@ -8,17 +8,17 @@ import myproviders
 
 def main():
     myproviders.Tools.cls()
-    print(myproviders.Tools.string_fill(u'>> LOF SPIDER START SPIDERING ', u'>', True))
-    print(myproviders.Tools.string_fill(u'>> 完全备份老福特Tag档案·排版导出 ', u'>', True))
-    print(myproviders.Tools.string_fill(u'>> 版本 %s ' % config.app_version, u'>', True))
-    options = (u'> 1. 将指定tag归档备份到本地数据库',
-         u'> 2. 更新指定tag的本地数据库',
-         u'> 3. 将数据库中的图片型博文筛选后导出',
-         u'> 4. 将数据库中的文档型博文筛选后导出',
-         u'> 5. 将数据库中的文档型博文筛选后制作成epub电子书')
+    print(myproviders.Tools.string_fill(u' LOF SPIDER START SPIDERING ', u'>', True))
+    print(myproviders.Tools.string_fill(u' 完全备份老福特Tag档案·筛选屏蔽·排版导出', u'>', True))
+    print(myproviders.Tools.string_fill(u' 版本 %s ' % config.app_version, u'>', True))
+    options = (u'1. 将指定tag归档备份到本地数据库',
+         u'2. 更新指定tag的本地数据库',
+         u'3. 将数据库中的图片型博文筛选后导出',
+         u'4. 将数据库中的文档型博文筛选后导出',
+         u'5. 将数据库中的文档型博文筛选后制作成epub电子书')
     options = dict(zip(('1','2','3','4','5'), options))
     #print('\n'.join(options.values()))
-    print(myproviders.Tools.string_fill(u'>> 备注: 请在config.py中修改配置信息.', r'>', True))
+    print(myproviders.Tools.string_fill(u' 备注: 请在config.py中修改配置信息. ', r'>', True))
     print(myproviders.Tools.string_fill(u'', r'>'))
     
     while True:
@@ -56,12 +56,12 @@ def main():
 
 def userInput(options):
     while True:
-        tagName = input(u'\n>> 请指定tag(仅一个): ' )
+        tagName = input(u'\n>> 请指定tag(仅一个): \n>> ' )
         if not tagName:
             print(u'>> tag不能为空，请重试！')
             continue
         print(u'>> 已指定tag: ', tagName)
-        certain = input(u'>> 是否确定？y-是 (others)-否: ')
+        certain = input(u'>> 是否确定？y-是 (others)-否: \n>> ')
         if certain in (u'y','Y'):
             print(u'>> 设置tag成功！当前工作tag: ', tagName)
             break
@@ -72,13 +72,13 @@ def userInput(options):
     while True:
         print('\n') 
         print('\n'.join(options.values()))
-        option = input(u'>> 请选择功能: ' )
+        option = input(u'>> 请选择功能: \n>> ' )
         if option not in options:
             print(u'>> 输入无效选择，请重试！')
             continue
 
         print(u'>> 已选择功能: \n',options[option] )
-        certain = input(u'>> 是否确定？y-是 (others)-否: ')
+        certain = input(u'>> 是否确定？y-是 (others)-否: \n>> ')
         if certain in ('y','Y'):
             break
         print(u'>> 重新选择...')
